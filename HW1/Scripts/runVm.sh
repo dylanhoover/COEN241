b20fae4b1cfd579ccdff28c6faded94058aa2724
dylanhoover <dylangranthoover@gmail.com>
2022-04-18 15:52:41 -0700

    Update runVm.sh

--- HW1/Scripts/runVm.sh
+++ HW1/Scripts/runVm.sh
@@ -1,25 +1,21 @@
 
 #!/bin/bash
 
-#Arg = 0 - low-end config
-#Arg = 1 - middle-end config
-#Arg = 2 - high-end config
+config=$1
 
-vm_config=$1
-
-if [ $vm_config -eq 0 ];
+if [ $config -eq 0 ];
 then
     sudo qemu-system-x86_64 -m 2048 -boot d -accel kvm -smp cores=1,threads=2 -boot strict=on -hda ubuntu.img
 
-elif [ $vm_config -eq 1 ];
+elif [ $config -eq 1 ];
 then
     sudo qemu-system-x86_64 -m 4096 -boot d -accel kvm -smp cores=2,threads=2 -boot strict=on -hda ubuntu.img
 
-elif [ $vm_config -eq 2 ];
+elif [ $config -eq 2 ];
 then
     sudo qemu-system-x86_64 -m 8192 -boot d -accel kvm -smp cores=4,threads=2 -boot strict=on -hda ubuntu.img
 
 else
-    echo "Incorrect VM Configuration selected."
+    echo "Error"
     exit 1
 fi
