c69512aa436aa99bad2c5ba7f38dbd88049aa9ba
dylanhoover <dylangranthoover@gmail.com>
2022-04-18 17:22:13 -0700

    Update runVm.sh
    Updating threads

--- HW1/Scripts/runVm.sh
+++ HW1/Scripts/runVm.sh
@@ -9,11 +9,11 @@ then
 
 elif [ $config -eq 1 ];
 then
-    sudo qemu-system-x86_64 -m 4096 -boot d -accel kvm -smp cores=2,threads=2 -boot strict=on -hda ubuntu.img
+    sudo qemu-system-x86_64 -m 4096 -boot d -accel kvm -smp cores=2,threads=4 -boot strict=on -hda ubuntu.img
 
 elif [ $config -eq 2 ];
 then
-    sudo qemu-system-x86_64 -m 8192 -boot d -accel kvm -smp cores=4,threads=2 -boot strict=on -hda ubuntu.img
+    sudo qemu-system-x86_64 -m 8192 -boot d -accel kvm -smp cores=4,threads=8 -boot strict=on -hda ubuntu.img
 
 else
     echo "Error"
